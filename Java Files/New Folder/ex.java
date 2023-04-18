1fa40bcae7d42b7d3d60032220f9226c88b72e15
Muaña, Allain Rico <113710322+AllainRico@users.noreply.github.com>
2023-04-18 14:04:47 +0800

    Delete Java Files directory

--- Java Files/New Folder/ex.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class ex{
-   public static void main(String[] args){
-   Scanner s = new Scanner(System.in);
-      String number = "213230";
-      
-      
-   }
-}
--- Java Files/order/ShippedOrder.java
+++
@@ -1,6 +0,0 @@
-public class ShippedOrder extends Order{
-   @Override
-   public double computeTotal(double quantity, double unitPrice){
-      return (quantity * unitPrice) + 4.00;
-   }
-}
